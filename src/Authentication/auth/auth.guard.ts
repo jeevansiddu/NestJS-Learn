2b5a9f797fc399f090c1aa9da8fe3d035126f8cb
jeevansiddu <jeevansiddusb@gmail.com>
2023-07-26 22:37:35 +0530

    updates

---
+++ src/Authentication/auth/auth.guard.ts
@@ -0,0 +1,46 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    console.log('token is ' + token);
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: jwtConstants.secret,
+      });
+      // 💡 We're assigning the payload to the request object here
+      // so that we can access it in our route handlers
+      //   const payload = 'hello jeevzn';
+      console.log(' request = ' + payload);
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    // console.log(request.headers);
+
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    const val = type === 'Bearer' ? token : undefined;
+    // console.log(val);
+    return val;
+  }
+}
